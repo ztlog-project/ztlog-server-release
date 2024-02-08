7ffee85ff1ed5dc7899374f55eae7008be3c8706
tromm <chldpwl5620@gmail.com>
2024-02-08 22:42:24 +0900

    fix: 스프링 시큐리티 적용

---
+++ ztlog-admin/src/main/java/com/devlog/admin/config/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.devlog.admin.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((csrfconfig) -> csrfconfig.disable())
+                .headers((headersConfig) -> headersConfig.frameOptions((frameOptionsConfig -> frameOptionsConfig.sameOrigin())))
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers("/v2/api-docs/**", "/swagger-resources/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
