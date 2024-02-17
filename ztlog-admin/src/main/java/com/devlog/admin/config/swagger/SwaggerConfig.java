5dfa7c01eab8fac43eaceead8108ef6855fce1db
tromm <chldpwl5620@gmail.com>
2024-02-17 20:56:46 +0900

    fix: 어드민 스웨거 주석 수정

--- ztlog-admin/src/main/java/com/devlog/admin/config/swagger/SwaggerConfig.java
+++ ztlog-admin/src/main/java/com/devlog/admin/config/swagger/SwaggerConfig.java
@@ -17,8 +17,8 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .title("ZTLog Front API Document")
+                .description("ZTlog Admin API 명세서입니다.")
                 .version("1.0.0");
     }
 }
