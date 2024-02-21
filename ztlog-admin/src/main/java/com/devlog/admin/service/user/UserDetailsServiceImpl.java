1a92633dc85bf60f7e6d1554567561cef7c456d4
tromm <chldpwl5620@gmail.com>
2024-02-22 00:13:36 +0900

    fix: rest template 빈 등록

--- ztlog-admin/src/main/java/com/devlog/admin/service/user/UserDetailsServiceImpl.java
+++ ztlog-admin/src/main/java/com/devlog/admin/service/user/UserDetailsServiceImpl.java
@@ -21,22 +21,22 @@ import java.util.Collections;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        // 1. userRepository로부터 loginId로 유저정보를 받아온다.
+        // userRepository 에서 유저 객체를 받아온다.
         User user = userRepository.findByUsername(userName);
         if (ObjectUtils.isEmpty(user))
             throw new DataNotFoundException(ResponseCode.NOT_FOUND_DATA.getMessage());
 
-        // 2.user를 dto로 변환시켜준다.
+        // user -> dto
         UserInfoDto userInfo = UserInfoDto.builder().build();
         BeanUtils.copyProperties(user, userInfo);
 
-        // 3. 사용자 정보를 기반으로 SecurityUserDetailsDto 객체를 생성한다.
+        // UserDetailDto 객체를 생성하여 반환
         return new UserDetailDto(userInfo,Collections.singleton(new SimpleGrantedAuthority(userInfo.getGrant())));
     }
 }
--- ztlog-core/src/main/java/com/devlog/core/config/CoreConfig.java
+++ ztlog-core/src/main/java/com/devlog/core/config/CoreConfig.java
@@ -3,6 +3,7 @@ package com.devlog.core.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class CoreConfig {
@@ -14,6 +15,15 @@ public class CoreConfig {
     @Bean
     public Pbkdf2PasswordEncoder passwordEncoder() {
         return new Pbkdf2PasswordEncoder("secret", 16, 310000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA512);
+    }
+
+    /**
+     * rest template
+     * @return  rest template
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
