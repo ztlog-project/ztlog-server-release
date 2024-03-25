e8738ed1d97c1e5c54e078ff874bc7a8f8ba8004
tromm <chldpwl5620@gmail.com>
2024-03-25 23:54:31 +0900

    fix: 컨텐츠 태그 주요키 Id 지정

--- ztlog-core/src/main/java/com/devlog/core/entity/content/ContentTagPK.java
+++ ztlog-core/src/main/java/com/devlog/core/entity/content/ContentTagPK.java
@@ -1,13 +1,23 @@
 package com.devlog.core.entity.content;
 
+import jakarta.persistence.Id;
 import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
 
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class ContentTagPK {
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContentTagPK implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = -8101983627354224421L;
+
+    @Id
     private Long tags;
+
+    @Id
     private Long contents;
 }
