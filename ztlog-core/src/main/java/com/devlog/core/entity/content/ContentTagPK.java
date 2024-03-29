76bfd207488e2f3437d3227ce2ed678e50dbcda3
tromm <chldpwl5620@gmail.com>
2024-03-29 16:30:47 +0900

    fix: ContentTagPK @Id 주석 제거

--- ztlog-core/src/main/java/com/devlog/core/entity/content/ContentTagPK.java
+++ ztlog-core/src/main/java/com/devlog/core/entity/content/ContentTagPK.java
@@ -14,9 +14,7 @@ public class ContentTagPK implements Serializable {
     @Serial
     private static final long serialVersionUID = -8101983627354224421L;
 
-//    @Id
     private Long tags;
 
-//    @Id
     private Long contents;
 }
