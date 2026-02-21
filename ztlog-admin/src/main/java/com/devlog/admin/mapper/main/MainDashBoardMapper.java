9e6d960c19c820b249dd6a43c9cd4036c101ea8c
tromm <chldpwl5620@gmail.com>
2026-02-21 19:55:56 +0900

    feat: 대쉬보드 응답 없을 경우 예외처리

--- ztlog-admin/src/main/java/com/devlog/admin/mapper/main/MainDashBoardMapper.java
+++ ztlog-admin/src/main/java/com/devlog/admin/mapper/main/MainDashBoardMapper.java
@@ -3,7 +3,9 @@ package com.devlog.admin.mapper.main;
 import com.devlog.admin.dto.main.request.MainStatisticsDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Optional;
+
 @Mapper
 public interface MainDashBoardMapper {
-    MainStatisticsDto selectMainStatistics();
+    Optional<MainStatisticsDto> selectMainStatistics();
 }
--- ztlog-admin/src/main/java/com/devlog/admin/service/main/MainDashBoardService.java
+++ ztlog-admin/src/main/java/com/devlog/admin/service/main/MainDashBoardService.java
@@ -3,6 +3,8 @@ package com.devlog.admin.service.main;
 import com.devlog.admin.mapper.main.MainDashBoardMapper;
 import com.devlog.admin.dto.main.request.MainStatisticsDto;
 import com.devlog.admin.dto.main.response.MainDashBoardResDto;
+import com.devlog.core.common.enumulation.ResponseCode;
+import com.devlog.core.config.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,7 +24,8 @@ public class MainDashBoardService {
      * @return 메인화면(대쉬보드) 정보
      */
     public MainDashBoardResDto getMainStatisticsInfo() {
-        MainStatisticsDto mainStatisticsDto = mainDashboardMapper.selectMainStatistics();
+        MainStatisticsDto mainStatisticsDto = mainDashboardMapper.selectMainStatistics()
+                .orElseThrow(() -> new DataNotFoundException(ResponseCode.NOT_FOUND_DATA.getMessage()));
         return MainDashBoardResDto.of(mainStatisticsDto);
     }
 
